7972c1c591572b2c2858ec39a17642c11a3af92f
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-04-18 23:36:11 +0600

    service detail edit

--- src/Pages/Home/ServiceDetail/ServiceDeatail.js
+++ src/Pages/Home/ServiceDetail/ServiceDeatail.js
@@ -5,7 +5,7 @@ const ServiceDeatail = () => {
     const { serviceId } = useParams();
     return (
         <div>
-            <h2 className='m-2'>Welcome to detail: {serviceId}</h2>
+            <h2 className='m-2'>Thank you for choosing us</h2>
             <div className='text-center m-2'>
                 <Link to="/checkout">
                     <button className='btn btn-dark'>Proceed Checkout</button>
