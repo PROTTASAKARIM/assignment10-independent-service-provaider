895e722cc02859b67a690642ef64749fe65e1e14
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-04-18 23:53:35 +0600

    final

---
+++ src/Pages/Home/Extra/Extra.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import mypic from "../../../images/MyPicture.jpg"
+
+const Extra = () => {
+    return (
+        <div className='container m-5'>
+            <h1 className='m-5'>Hi I am the owner of the page</h1>
+            <div className='d-flex nm-2'>
+                <img src={mypic} alt="" />
+                <div className='container m-2'>
+                    <h3>Contact Me</h3>
+                    <p>Hi I am Prottasa Karim.Please contact me If you want any think to in form. I am the owner of Dreamy Photography.</p>
+                    <p>My mail address is prottasakarim99@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Extra;
